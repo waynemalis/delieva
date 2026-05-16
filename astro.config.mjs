3e876af1ecf7eb1109ce5e51fa11bdee1cbeb58d
waynemalis <jesusortiz2050@gmail.com>
2026-05-16 14:45:22 -0600

    1.9

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,12 +3,13 @@ import { defineConfig } from 'astro/config';
 
 import tailwindcss from '@tailwindcss/vite';
 
+import sitemap from '@astrojs/sitemap';
+
 import react from '@astrojs/react';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://TU-USUARIO.github.io',
-  base: '/delieva',  // nombre exacto de tu repositorio
+  site: 'https://www.botanasdelieva.com',
   
   vite: {
     plugins: [tailwindcss()],
@@ -21,7 +22,7 @@ export default defineConfig({
     allowedHosts: true
   },
 
-  integrations: [react()],
+  integrations: [react(), sitemap()],
 
   i18n: {
     defaultLocale: 'es',
